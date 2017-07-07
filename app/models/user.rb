6ea39bd1bf570da3a7947f4000ef8a2a53bcd3f5
Nick <komarnickolas@gmail.com>
2017-07-07 17:56:18 -0600

    Update

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+  has_many :comments
+
+  has_and_belongs_to_many :friendships, class_name: "User", join_table: :friendships, foreign_key: :user_id, association_foreign_key: :friend_user_id
+
+  def full_name
+    first_name + " " + last_name
+  end
+end
